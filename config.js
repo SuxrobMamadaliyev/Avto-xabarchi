b5b5b9a260a071d347c23698a240e2c0e21de3e4
SuxrobMamadaliyev <mamadaliyevsuhrob69@gmail.com>
2026-07-04 16:45:00 +0500

    Modify bot token and storage channel username
    Updated BOT_TOKEN and STORAGE_CHANNEL_USERNAME in config.

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 // ========== KONFIGURATSIYA ==========
 const config = {
   ADMIN_ID: 123456789,               // O'zingizning Telegram ID'ingiz
-  BOT_TOKEN: "YOUR_BOT_TOKEN",       // @BotFather dan olingan token
+  BOT_TOKEN: "8928608829:AAFArHhhYw0NZWYt6_F9nLu-mzjR3f6GUAs",       // @BotFather dan olingan token
   API_ID: 12345678,                  // my.telegram.org dan
   API_HASH: "de4b653676e085ce3d0f3d64f8741ae4",
-  STORAGE_CHANNEL_USERNAME: "@ajskhdgjasduouwqyuvdqhuq",
+  STORAGE_CHANNEL_USERNAME: "@",
   DB_FILE: "telegram_bot.db",
   SESSIONS_DIR: "sessions",
   MIN_INTERVAL: 20,    // daqiqa
