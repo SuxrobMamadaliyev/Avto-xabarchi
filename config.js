6ca8e63404d1956c76c2a46f965417e283c325d5
SuxrobMamadaliyev <mamadaliyevsuhrob69@gmail.com>
2026-07-04 16:31:49 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+// ========== KONFIGURATSIYA ==========
+const config = {
+  ADMIN_ID: 123456789,               // O'zingizning Telegram ID'ingiz
+  BOT_TOKEN: "YOUR_BOT_TOKEN",       // @BotFather dan olingan token
+  API_ID: 12345678,                  // my.telegram.org dan
+  API_HASH: "de4b653676e085ce3d0f3d64f8741ae4",
+  STORAGE_CHANNEL_USERNAME: "@ajskhdgjasduouwqyuvdqhuq",
+  DB_FILE: "telegram_bot.db",
+  SESSIONS_DIR: "sessions",
+  MIN_INTERVAL: 20,    // daqiqa
+  MAX_INTERVAL: 25,    // daqiqa
+  RANDOM_MESSAGES: true,
+};
+
+module.exports = config;
